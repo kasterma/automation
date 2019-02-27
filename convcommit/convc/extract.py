30e27c98c7d24e96cd786e90ea581c3793ac4337
Bart Kastermans <kasterma@kasterma.net>
2019-02-27 22:42:41 +0100

    feat (convcommit): parsing all parts of the conventional commit

--- convcommit/convc/extract.py
+++ convcommit/convc/extract.py
@@ -1,10 +1,34 @@
+import re
+
+type_scope_re = re.compile(r"([^(]+)\(([^)]+)\)")
+
+
 def extract(commitm):
-    commitm = commitm.split('\n')
+    commitm = commitm.split('\n', 1)
     header = commitm[0].split(":")
-    rv = {
-        'type': header[0] if len(header) > 1 else "",
-        'description': header[1].strip() if len(header) > 1 else header[0],
-        'body': "",
-        'footer': ""
-    }
+    body_and_footer = "" if len(commitm) == 1 else commitm[1]
+    breaking = any(l.startswith("BREAKING CHANGE:") for l in body_and_footer.split("\n"))
+    if len(header) > 1:
+        scope_match = type_scope_re.match(header[0])
+        if scope_match:
+            commit_type = scope_match.group(1).strip()
+            commit_scope = scope_match.group(2)
+        else:
+            commit_type = header[0]
+            commit_scope = None
+        rv = {
+            'type': commit_type,
+            'scope': commit_scope,
+            'description': header[1].strip() if len(header) > 1 else header[0],
+            'body_and_footer': body_and_footer,
+            'breaking': breaking
+        }
+    else:
+        rv = {
+            'type': None,
+            'scope': None,
+            'description': header[0],
+            'body_and_footer': body_and_footer,
+             'breaking': breaking
+        }
     return rv
--- convcommit/tests/test_extract.py
+++ convcommit/tests/test_extract.py
@@ -4,6 +4,42 @@ from convc import extract
 def test_extract1():
     e1 = extract("feat: feature X fixed")
     assert e1['type'] == "feat"
+    assert e1['scope'] is None
     assert e1['description'] == "feature X fixed"
-    assert e1['body'] == ""
-    assert e1['footer'] == ""
+    assert e1['body_and_footer'] == ""
+    assert e1['breaking'] == False
+
+
+def test_extract2():
+    e1 = extract("feat: feature X fixed\n\nbody body\n\nfooterfooter\n")
+    assert e1['type'] == "feat"
+    assert e1['scope'] is None
+    assert e1['description'] == "feature X fixed"
+    assert e1['body_and_footer'] == "\nbody body\n\nfooterfooter\n"
+    assert e1['breaking'] == False
+
+
+def test_extract3():
+    e1 = extract("feat: feature X fixed\n\nbody body\n\nfooterfooter\n")
+    assert e1['type'] == "feat"
+    assert e1['scope'] is None
+    assert e1['description'] == "feature X fixed"
+    assert e1['body_and_footer'] == "\nbody body\n\nfooterfooter\n"
+    assert e1['breaking'] == False
+
+
+def test_extract4():
+    e1 = extract("feat: feature X fixed\n\nbody body\n\nBREAKING CHANGE: footerfooter\n")
+    assert e1['type'] == "feat"
+    assert e1['scope'] is None
+    assert e1['description'] == "feature X fixed"
+    assert e1['body_and_footer'] == "\nbody body\n\nBREAKING CHANGE: footerfooter\n"
+    assert e1['breaking'] == True
+
+def test_extract5():
+    e1 = extract("feat(ssccooppee): feature X fixed")
+    assert e1['type'] == "feat"
+    assert e1['scope'] == "ssccooppee"
+    assert e1['description'] == "feature X fixed"
+    assert e1['body_and_footer'] == ""
+    assert e1['breaking'] == False
